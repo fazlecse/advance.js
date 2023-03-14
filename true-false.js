11bb4ccb1bba75d2d36c4cfc69b0316ec8960fd4
Md. Fazle Rabbi <mdfazlerabbi06@gmail.com>
2023-03-15 00:35:18 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,21 @@
+// Falsy
+// 0
+// ""
+// undefined
+// null
+// NaN
+// false 
+// Truthy
+// '0'
+// ''
+// []
+// {}
+
+// const age = 0;
+const name = '';
+
+if(name){
+    console.log('Conditon is true');
+}else{
+    console.log('Condition is false');
+}
